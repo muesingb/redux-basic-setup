55697e2d0b6db919d4ab4edd318cc63483108b28
Barbara <muesingb@gmail.com>
2020-05-17 18:45:33 -0400

    edited app js file to include provider

--- src/App.js
+++ src/App.js
@@ -1,29 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-/*functional component*/
 import configureStore from './src/store/configureStore';
 import { Provider } from 'react-redux';
 
@@ -31,8 +6,7 @@ const App = () => {
 
   return (
     <Provider store={configureStore} >
-      <Navigator />
-      {/* <TabNavigator /> */}
+      <ApplicationComponents />
     </Provider>
   );
 };
